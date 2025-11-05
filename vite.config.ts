f4af241427aed1269c5602025621e86d37687085
Vasanth Gondrala <vasanthvs.7032@gmail.com>
2025-11-05 18:11:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 
   import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+
+  export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true,
+    },
+  });
 
   export default defineConfig({
     plugins: [react()],
